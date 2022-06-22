9a4abc3300e7aa0670ad676f9bb43ff3b278cc62
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2022-06-22 11:32:38 +0800

    added interface to main
    + updated gitignore

--- main.py
+++ main.py
@@ -14,12 +14,50 @@ def vigenere_encrypt(message, key):
 
 
 # decrypts message using the Vigenere cipher
-def vigenere_encrypt(message, key):
+def vigenere_decrypt(message, key):
     pass
 
 
 def main():
-    pass
+    # user main menu
+    while True:
+        try:
+            print("1. Caesar cipher encrypt")
+            print("2. Caesar cipher decrypt")
+            print("3. Vigenere cipher encrypt")
+            print("4. Vigenere cipher decrypt")
+
+            selection = int(input("Select function. Enter number to continue. \n"))
+
+        # invalid input
+        except ValueError:
+            print("Invalid input. Please try again.")
+
+        # valid input
+        else:
+            # parse user's decision
+            if selection == 1:
+                message = input("Enter message: ")
+                key = input("Enter key: ")
+                caesar_encrypt(message, key)
+
+            elif selection == 2:
+                message = input("Enter message: ")
+                key = input("Enter key: ")
+                caesar_decrypt(message, key)
+
+            elif selection == 3:
+                message = input("Enter message: ")
+                key = input("Enter key: ")
+                vigenere_encrypt(message, key)
+
+            elif selection == 4:
+                message = input("Enter message: ")
+                key = input("Enter key: ")
+                vigenere_decrypt(message, key)
+
+            else:
+                print("Invalid input. Please try again.")
 
 
 if __name__ == "__main__":
