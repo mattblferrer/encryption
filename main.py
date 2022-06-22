bf6dffb89f3aadfad23c16eeee3e90f04ddbeb19
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2022-06-22 11:15:23 +0800

    added main template

---
+++ main.py
@@ -0,0 +1,26 @@
+# encrypts message using the Caesar cipher
+def caesar_encrypt(message, key):
+    pass
+
+
+# decrypts message using the Caesar cipher
+def caesar_decrypt(message, key):
+    pass
+
+
+# encrypts message using the Vigenere cipher
+def vigenere_encrypt(message, key):
+    pass
+
+
+# decrypts message using the Vigenere cipher
+def vigenere_encrypt(message, key):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
